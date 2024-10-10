647aa9ba1e87ea74479bd50f1da22dd0906ae0b5
Sean Marinas <seanmarinas@gmail.com>
2024-10-11 05:54:00 +0800

    Replaced generic app name

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -15,9 +15,9 @@ export default function LoginPage() {
     <section className="flex h-full w-full justify-center align-middle items-center">
       <Card className="p-[1rem] w-fit">
         <CardHeader className="text-center">
-          <CardTitle className="text-2xl">Sign in to AppName</CardTitle>
+          <CardTitle className="text-2xl">Sign in to Beanaby</CardTitle>
           <CardDescription>
-            Welcome to AppName! Please enter your log in details to get started
+            Welcome to Beanaby! Please enter your log in details to get started
           </CardDescription>
         </CardHeader>
 
