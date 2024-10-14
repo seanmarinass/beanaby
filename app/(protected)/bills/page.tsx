e7f8af84897daf9ed5da6c4f06bce1f646031263
Sean Marinas <seanmarinas@gmail.com>
2024-10-15 03:36:25 +0800

    refactored bills page

--- app/(protected)/bills/page.tsx
+++ app/(protected)/bills/page.tsx
@@ -3,8 +3,15 @@ import BillDetailsSection from "./BillDetailsSection";
 import BillsOverviewSection from "./BillsOverviewSection";
 import { BillDto } from "@/lib/dtos";
 import FadeIn from "@/components/fadeIn";
+import { useSession } from "next-auth/react";
+import { Alert } from "@/components/ui/alert";
+import { auth } from "@/auth";
 
 export default async function BillsPage() {
+  const session = await auth();
+  if (!session?.user?.email) return <Alert>No email</Alert>;
+
+  const billList = await userBillList(session.user.email!);
   return (
     <FadeIn
       className="flex flex-grow h-full w-full gap-[1rem] items-center"
@@ -12,7 +19,7 @@ export default async function BillsPage() {
       delay={0.3}
     >
       <div className="flex w-1/3 h-full">
-        <BillsOverviewSection billList={[]} />
+        <BillsOverviewSection billList={billList} />
       </div>
 
       <div className="flex w-2/3 h-full">
